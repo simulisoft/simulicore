cb14ad23ff84a42f6e0c4a31f4736bd78581a244
Connor <connor@simulisoft.com>
2026-01-10 14:04:23 +0200

    cuLite - Update tutorial & small examples CMake change

--- culite.mod/documentation/sectionfixer.sh
+++ culite.mod/documentation/sectionfixer.sh
@@ -3,5 +3,7 @@
 ROOT_DIR=`dirname $0`
 
 sed -i 's/#culite_tutorial_datatypes\>//g' ${ROOT_DIR}/html/*.js
+sed -i 's/#culite_tutorial_vectors\>//g' ${ROOT_DIR}/html/*.js
+sed -i 's/#culite_tutorial_vectors_dense\>//g' ${ROOT_DIR}/html/*.js
 
 exit 0
