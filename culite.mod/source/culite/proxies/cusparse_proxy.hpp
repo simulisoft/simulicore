2397eef01f12e39b1f5a4fee7e5f6557369f90b0
Connor <connor@simulisoft.com>
2026-01-23 09:46:56 +0200

    cuLite - Add geam2 proxies (cuSparse)
      Add the legacy geam2 functionality. Unfortunately the legacy interface
      supports only 32bit integers. Will wait for a generic API that handles
      sparse matrix addition. Until then, we can transform 64bit integers to
      32bit integers, call geam2 and then get the result back to int64

--- culite.mod/source/culite/proxies/cusparse_proxy.hpp
+++ culite.mod/source/culite/proxies/cusparse_proxy.hpp
@@ -55,6 +55,106 @@ inline constexpr cusparseIndexType_t indexType()
 
 /*-------------------------------------------------*/
 
+#if !defined (CULITE_I64)
+
+class LegacyMatDescr {
+
+    public:
+        LegacyMatDescr()
+        {
+            cusparseStatus_t cusparseStatus = cusparseCreateMatDescr(&m_descr);
+            err::check_cusparse(cusparseStatus);
+        }
+
+        ~LegacyMatDescr()
+        {
+            cusparseStatus_t cusparseStatus = cusparseDestroyMatDescr(m_descr);
+            err::check_cusparse(cusparseStatus);
+        }
+
+        cusparseMatDescr_t descr() const { return m_descr; }
+
+    private:
+        cusparseMatDescr_t m_descr{nullptr};
+};
+
+//
+// At the moment only 32bit integers are supported by legacy cuSparse
+//
+#define csr_geam2_reserve_macro(typein) \
+void csr_geam2_reserve(cusparseHandle_t         handle, \
+                       int_t                    m, \
+                       int_t                    n, \
+                       const typein*            alpha, \
+                       const cusparseMatDescr_t descrA, \
+                       int_t                    nnzA, \
+                       const typein*            csrSortedValA, \
+                       const int_t*             csrSortedRowPtrA, \
+                       const int_t*             csrSortedColIndA, \
+                       const typein*            beta, \
+                       const cusparseMatDescr_t descrB, \
+                       int_t                    nnzB, \
+                       const typein*            csrSortedValB, \
+                       const int_t*             csrSortedRowPtrB, \
+                       const int_t*             csrSortedColIndB, \
+                       const cusparseMatDescr_t descrC, \
+                       const typein*            csrSortedValC, \
+                       const int_t*             csrSortedRowPtrC, \
+                       const int_t*             csrSortedColIndC, \
+                       size_t*                  pBufferSizeInBytes)
+csr_geam2_reserve_macro(real_t);
+csr_geam2_reserve_macro(real4_t);
+csr_geam2_reserve_macro(complex_t);
+csr_geam2_reserve_macro(complex8_t);
+#undef csr_geam2_reserve_macro
+
+void csr_geam2_nnz(cusparseHandle_t         handle,
+                   int_t                    m,
+                   int_t                    n,
+                   const cusparseMatDescr_t descrA,
+                   int_t                    nnzA,
+                   const int_t*             csrSortedRowPtrA,
+                   const int_t*             csrSortedColIndA,
+                   const cusparseMatDescr_t descrB,
+                   int_t                    nnzB,
+                   const int_t*             csrSortedRowPtrB,
+                   const int_t*             csrSortedColIndB,
+                   const cusparseMatDescr_t descrC,
+                   int_t*                   csrSortedRowPtrC,
+                   int_t*                   nnzTotalDevHostPtr,
+                   void*                    workspace);
+
+#define csr_geam2_macro(typein) \
+void csr_geam2(cusparseHandle_t            handle, \
+                  int_t                    m, \
+                  int_t                    n, \
+                  const typein*            alpha, \
+                  const cusparseMatDescr_t descrA, \
+                  int_t                    nnzA, \
+                  const typein*            csrSortedValA, \
+                  const int_t*             csrSortedRowPtrA, \
+                  const int_t*             csrSortedColIndA, \
+                  const typein*            beta, \
+                  const cusparseMatDescr_t descrB, \
+                  int_t                    nnzB, \
+                  const typein*            csrSortedValB, \
+                  const int_t*             csrSortedRowPtrB, \
+                  const int_t*             csrSortedColIndB, \
+                  const cusparseMatDescr_t descrC, \
+                  typein*                  csrSortedValC, \
+                  int_t*                   csrSortedRowPtrC, \
+                  int_t*                   csrSortedColIndC, \
+                  void*                    pBuffer)
+csr_geam2_macro(real_t);
+csr_geam2_macro(real4_t);
+csr_geam2_macro(complex_t);
+csr_geam2_macro(complex8_t);
+#undef csr_geam2_macro
+
+#endif // CULITE_I64
+
+/*-------------------------------------------------*/
+
 template <typename T_Scalar>
 class DnVec {
 
@@ -123,7 +223,6 @@ class SpMatBase {
 
     public:
         SpMatBase() = default;
-        
         ~SpMatBase()
         {
             if(m_descr != nullptr) {
