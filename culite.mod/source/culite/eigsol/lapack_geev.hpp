bf121ac8d87548fc51e4530bf063a0f1cfbe7bc3
Connor <connor@simulisoft.com>
2026-02-23 20:21:02 +0200

    cuLite - Add documentation for LapackGeev()

--- culite.mod/source/culite/eigsol/lapack_geev.hpp
+++ culite.mod/source/culite/eigsol/lapack_geev.hpp
@@ -21,11 +21,7 @@
  * @file
  */
 
-//#include <cla3p/types/enums.hpp>
-//#include <cla3p/generic/meta2d.hpp>
-//
 #include "culite/support/imalloc.hpp"
-//#include "culite/types/integer.hpp"
 #include "culite/generic/cusolver_handler.hpp"
 
 /*-------------------------------------------------*/
@@ -36,6 +32,30 @@ namespace culite {
 
 /**
  * @nosubgrouping
+ * @brief Eigenvalue decomposition solver using LAPACK/cuSOLVER GEEV routines.
+ * @details This class provides a high-level interface for computing eigenvalues and eigenvectors
+ *          of general (non-symmetric) matrices using the cuSOLVER library's GEEV implementation.
+ *          
+ *          The GEEV routine computes the eigenvalue decomposition of a general matrix A:
+ *          - Right eigenvectors: @f$ A v_i = \lambda_i v_i @f$
+ *          - Left eigenvectors: @f$ w_i^H A = \lambda_i w_i^H @f$
+ * 
+ *          Results are always returned as complex matrices/vectors, even for real input matrices.
+ * 
+ * @tparam T_Matrix The matrix type (can be real or complex).
+ * 
+ * @section geev_usage Usage Example
+ * @code
+ * CuSolverHandler cusolver;
+ * culite::dns::RdMatrix A = ...; // Input matrix
+ * 
+ * LapackGeev<culite::dns::RdMatrix> geev(cusolver, false, true);
+ * geev.reserve(A);
+ * geev.decompose(A);
+ * 
+ * auto eigenvalues = geev.eigenvalues();
+ * auto rightEigenvectors = geev.rightEigenvectors();
+ * @endcode
  */
 template <typename T_Matrix>
 class LapackGeev {
@@ -45,17 +65,73 @@ class LapackGeev {
         using T_CScalar = typename TypeTraits<T_Scalar>::complex_type;
 
 	public:
+		/**
+		 * @brief Constructor.
+		 * @details Initializes the eigenvalue solver with the specified cuSOLVER handler
+		 *          and eigenvector computation options.
+		 * @param[in] cusolver Reference to a cuSOLVER handler instance.
+		 * @param[in] calcLeft If true, computes left eigenvectors.
+		 * @param[in] calcRight If true, computes right eigenvectors.
+		 */
     	LapackGeev(CuSolverHandler& cusolver, bool calcLeft, bool calcRight);
+		
+		/**
+		 * @brief Destructor.
+		 * @details Releases all allocated resources and clears internal state.
+		 */
 		~LapackGeev();
 
+		/**
+		 * @brief Reserves workspace memory for eigenvalue decomposition.
+		 * @details Allocates internal buffers required for eigenvalue computation based on
+		 *          the dimensions of the input matrix. Must be called before @ref decompose.
+		 * @param[in] mat The matrix for which to reserve workspace (determines dimensions).
+		 */
 		void reserve(const T_Matrix& mat);
 
+		/**
+		 * @brief Clears all internal data and releases allocated memory.
+		 * @details Resets the solver to its initial state, releasing all computed results
+		 *          and internal workspace buffers.
+		 */
 		void clear();
 
+		/**
+		 * @brief Performs eigenvalue decomposition on the input matrix.
+		 * @details Computes the eigenvalues and optionally the left and/or right eigenvectors
+		 *          of the input matrix using cuSOLVER's GEEV routine.
+		 *          
+		 *          The input matrix is not modified. Results can be retrieved using the
+		 *          accessor methods: @ref eigenvalues, @ref leftEigenvectors, @ref rightEigenvectors.
+		 * 
+		 * @param[in] mat The matrix to decompose.
+		 * 
+		 * @pre @ref reserve must have been called with a matrix of matching dimensions.
+		 * @note For real input matrices, eigenvalues and eigenvectors are returned as complex.
+		 */
 		void decompose(const T_Matrix& mat);
 
+		/**
+		 * @brief Gets the computed eigenvalues.
+		 * @return Const reference to the complex vector containing the eigenvalues.
+		 * @note Only valid after calling @ref decompose.
+		 */
         const dns::CxVector<T_CScalar>& eigenvalues() const { return m_eigenvalues; }
+		
+		/**
+		 * @brief Gets the computed left eigenvectors.
+		 * @return Const reference to the complex matrix containing the left eigenvectors.
+		 * @note Only valid after calling @ref decompose with calcLeft=true.
+		 * @note Each column i contains the left eigenvector corresponding to eigenvalue i.
+		 */
         const dns::CxMatrix<T_CScalar>& leftEigenvectors() const { return m_leftEigenvectors; }
+		
+		/**
+		 * @brief Gets the computed right eigenvectors.
+		 * @return Const reference to the complex matrix containing the right eigenvectors.
+		 * @note Only valid after calling @ref decompose with calcRight=true.
+		 * @note Each column i contains the right eigenvector corresponding to eigenvalue i.
+		 */
         const dns::CxMatrix<T_CScalar>& rightEigenvectors() const { return m_rightEigenvectors; }
 
 	private:
